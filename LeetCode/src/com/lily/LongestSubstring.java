a7eadfbab3aa9b37bbdfb4cc9d1538f3e1b67fef
lily <lilinyu861@outlook.com>
2019-03-20 13:53:19 +0800

    求字符串最长不含重复字符的子字符串解题思路

--- LeetCode/src/com/lily/LongestSubstring.java
+++ LeetCode/src/com/lily/LongestSubstring.java
@@ -1,3 +1,11 @@
+/* *
+ * 题目：
+ * 给定一个字符串，找到最长子字符串的长度而不重复字符。
+ * 解题思路：
+ *      利用hashMap，将不存在于map中的字符存放到map中，然后比较max和hashMap的长度，去最长值
+ *      若字符已经在map中，则回到map中移除数据，从头开始移除。
+ *      比较完成之后最后的结果时hashMap和set相比的最大值。
+ */
 package com.lily;
 
 import java.util.HashMap;
